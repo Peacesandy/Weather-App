5e4b3bb616f9818318a4136f3965d66f05dc7ad6
unknown <peacesandy04@gmail.com>
2022-10-11 04:54:27 -0700

    Added variables to script file

---
+++ script.js
@@ -0,0 +1,10 @@
+let lon;
+let lat;
+let temperature = document.querySelector(".temp");
+let summary = document.querySelector(".summary");
+let loc = document.querySelector(".location");
+let icon = document.querySelector(".icon");
+const kelvin = 273;
+const api = "6d055e39ee237af35ca066f35474e9df";  
+
+const btn = document.querySelector(".btn")
