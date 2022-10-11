762cee62dc7d031476501fa7ccfebf8daac01019
unknown <peacesandy04@gmail.com>
2022-10-11 04:57:24 -0700

    Added renderdata to script.js

--- script.js
+++ script.js
@@ -8,3 +8,14 @@ const kelvin = 273;
 const api = "6d055e39ee237af35ca066f35474e9df";  
 
 const btn = document.querySelector(".btn")
+
+const renderData = (data) =>{
+    console.log(data);
+            temperature.textContent = 
+            Math.floor(data.main.temp - kelvin) + "°C";
+            summary.textContent = data.weather[0].description;
+            loc.textContent = data.name + "," + data.sys.country;
+            let icon1 = data.weather[0].icon;
+            icon.innerHTML = 
+     `<img src="https://img.icons8.com/color/2x/partly-cloudy-rain--v2.gif" style= 'height:10rem'/>`
+  }
